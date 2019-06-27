31a7d63723e6b3b065e1165feac4e4e31e708a54
Karina Kalpaxis <kalpaxis1@gmail.com>
2019-06-27 13:11:19 -0400

    Minor Details in Pages Added

---
+++ our-team.js
@@ -0,0 +1,12 @@
+const siteNav = document.querySelectorAll("nav a");
+siteNav[0].textContent = "Home";
+siteNav[1].textContent = "The App";
+siteNav[2].textContent = "The Story";
+siteNav[3].textContent = "The Team";
+siteNav[4].textContent = "Log In/Sign Up";
+
+const navOrange = document.querySelectorAll("nav a");
+for (let i = 0; i < nav.length; i++) {
+  navGreen[i].style.color = "orange";
+}
+
