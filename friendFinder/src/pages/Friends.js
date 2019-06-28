a0531dcc3dc270a43915e7927a107be942a3071f
Jonathan Calderaro <jcc2019.lambda@gmail.com>
2019-06-28 15:11:39 -0700

    Final Countdown

--- friendFinder/src/pages/Friends.js
+++ friendFinder/src/pages/Friends.js
@@ -1,31 +1,44 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import axios from "axios";
+import { Route, NavLink } from 'react-router-dom';
+import './App.css';
+import SmurfForm from './components/SmurfForm';
+import Smurfs from './components/Smurfs';
 
 class Friends extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      myfriends: []
+      smurfs: [],
     };
   }
 
   componentDidMount() {
-    this.fetchFriends();
+    axios
+      .get("http://localhost:3333/smurfs")
+      .then(res => this.setState({ smurfs: res.data }))
+      .catch(err => console.log(err));
+
   }
-
-  fetchFriends() {
-    window.axios.get(FriendList).then(response => {
-      console.log(response);
-
-      let myfriends = response.data.data;
-
-      this.setState({
-        myfriends: myfriends
-      });
-    });
-  }
-
   render() {
-    return <div>hey</div>;
+    return (
+
+      <div className="App">
+        <nav>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/form">New</NavLink>
+        </nav>
+
+        <Route
+          path="/form"
+          render={props => <SmurfForm {...props} getSmurfs={this.getSmurfs} />}
+        />
+        <Route
+          path="/"
+          render={props => <Smurfs {...props} smurfs={this.state.smurfs} />}
+        />
+      </div>
+    );
   }
 }
 
